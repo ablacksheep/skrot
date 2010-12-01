8d929fd0d5f3870866b07ec7238aef3ce7f9e5a2
Dahl <dahl_lia@hotmail.com>
2010-12-01 14:49:19 +0100

    hfiegh

--- src/hello.java
+++ src/hello.java
@@ -3,4 +3,7 @@ public class hello {
 	public static void main(String[]negr){
 		System.out.println("fy faen det opplegget her suger hestepikk. faens skrotmig");
 	}
+	public static int hore(){
+		return 666;
+	}
 }
