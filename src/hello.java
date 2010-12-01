bb1e14b71e3da806dea96ab34f021e77805fdfd2
Dahl <dahl_lia@hotmail.com>
2010-12-01 14:58:40 +0100

    huehg

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,16 @@
 
 public class hello {
 	public static void main(String[]negr){
-		System.out.println("fy faen det opplegget her suger hestepikk. faens skrotmig");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println("uansett hva dere skriver er jeg t�ffest! MOHAHA!");
+		System.out.println(tall());
+	}
+	public static int tall(){
+		return 666;
 	}
 	public static int hore(){
 		return 666;
